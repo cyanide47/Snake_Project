4c6a304ec183d0c7102d2cb5b4fcab1a01f0604d
cyanide47 <christopher.sainsaulieu@gmail.com>
2022-10-19 20:14:36 +0200

    demarrage du projet snake en c

---
+++ main.c
@@ -0,0 +1,21 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+
+#define COLS 60
+#define ROWS 30
+
+int main()
+{
+    int quit = 0;
+    while (!quit) {
+        // Render table
+        printf("|");
+        for (int i = 0; i < COLS; i++)
+            printf("-");
+        printf("|\n");
+
+    }
+
+    return 0;
+}
