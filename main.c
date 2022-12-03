5596ea933db6e6505f7b08f8cc8751ec84154b4d
cyanide47 <christopher.sainsaulieu@gmail.com>
2022-12-03 09:22:45 +0100

    Avancement du projet

--- main.c
+++ main.c
@@ -5,31 +5,46 @@
 #define COLS 60
 #define ROWS 30
 
-int main()
-{
+int main(){
+
+    // Hide cursor
+    printf("\e[?25l");
+
+    int x[1000], y[1000];
     int quit = 0;
     while (!quit) {
         // Render table
-        printf("|");
+        // First lines
+        printf("X");
         for (int i = 0; i < COLS; i++)
             printf("-");
-        printf("¬\n");
+        printf("X\n");
 
+        // middle lines
         for (int j = 0; j < ROWS; j++) {
             printf("|");
             for (int i = 0; i < COLS; i++)
                 printf(".");
             printf("|\n");
         }
-        
-        printf("L");
+
+        // End lines
+        printf("X");
         for (int i = 0; i < COLS; i++)
             printf("-");
-        printf("|\n");
+        printf("X\n");
 
-        // Moe cursor back to top
+        // Move cursor back to top
+        printf("", ROWS);
 
+        int head = 0, tail = 0;
+        x[head] = COLS/2;
+        y[head] = ROWS/2;
+        int gamover = 0;
+        int xdir = 1, ydir = 0;
     }
 
+    // Show cursor
+    printf("\e[?25h");
     return 0;
 }
